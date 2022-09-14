b23296162eba93c702942e54395ba5c0cc686fda
Priyank Vasoya <priyanksoftcolon@gmail.com>
2022-09-14 20:50:23 +0530

    WAL-11-KYC-performed

--- controllers/kyc/index.js
+++ controllers/kyc/index.js
@@ -2,14 +2,15 @@ import { User, Aadhar } from '../../model/index.js'
 
 export const doKyc = async (req, res) => {
   const id = req.params.aadharId
-  console.log(id)
-  const aadhar = await Aadhar.find(id)
-  console.log('Aadhar ', aadhar.userId)
+
+  const aadhar = await Aadhar.findOne({ _id: id })
+
   try {
-    if (req.body.userId == aadhar.userId) {
-      // const id = req.body.userId
+    if (req.body.userId === aadhar.userId) {
       await User.findByIdAndUpdate(req.body.userId, { $set: { kyc: true } })
       res.status(200).json('Kyc performed successfully')
+    } else {
+      res.status(400).json('Invalid credentials')
     }
   } catch (e) {
     res.status(500).json(e)
